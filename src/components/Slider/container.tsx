03e06871ba38fcf42ac60a93a37ed950d40571d2
muhammadtalha242 <m.talha2244@gmail.com>
2022-11-27 04:34:25 +0500

    tile styles added

--- src/components/Slider/container.tsx
+++ src/components/Slider/container.tsx
@@ -5,7 +5,6 @@ export const SliderContainer = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: ${GREEN_TERTIARY};
   position: relative;
 `;
 
@@ -25,4 +24,50 @@ export const SliderArrowContainer = styled.div<{ direction: "left" | "right" }>`
   right: ${(props) => props.direction === "right" && "10px"};
   cursor: pointer;
   opacity: 0.5;
+`;
+
+export const TileContainer = styled.div`
+  height: 100%;
+`;
+
+export const TileContentContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+
+export const TileImageContainer = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`;
+
+export const TileStyledImage = styled.img`
+  height: 80%;
+`;
+
+export const TileInfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+export const TileTitleContainer = styled.h1`
+  font-size: 70px;
+`;
+
+export const TileDescContainer = styled.p`
+  margin: 50px 0;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+
+export const TileButtonContainer = styled.button`
+  padding: 10px;
+  background-color: transparent;
+  font-size: 20px;
+  cursor: pointer;
 `;
